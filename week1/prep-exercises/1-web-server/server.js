dcad45da535aaabb9655afc8163b8cdd5f461bed
tenzijn <tenzinkunchok2022@gmail.com>
2024-01-26 22:26:30 +0100

    week1 nodejs completed

--- week1/prep-exercises/1-web-server/server.js
+++ week1/prep-exercises/1-web-server/server.js
@@ -3,12 +3,36 @@
  */
 
 const http = require('http');
+const fs = require('fs');
 
 //create a server
 let server = http.createServer(function (req, res) {
-  // YOUR CODE GOES IN HERE
-	res.write('Hello World!'); // Sends a response back to the client
-	res.end(); // Ends the response
+  if (req.url === '/') {
+    const filePath = path.join(__dirname, 'index.html');
+    fs.readFile(filePath, 'utf8', function (err, content) {
+      if (err) {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('File not found');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(content);
+      }
+    });
+  } else if (req.url === '/index.js') {
+    const filePath = path.join(__dirname, 'index.js');
+    fs.readFile(filePath, 'utf8', function (err, content) {
+      if (err) {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('File not found');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/javascript' });
+        res.end(content);
+      }
+    });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('File not found');
+  }
 });
 
 server.listen(3000); // The server starts to listen on port 3000
