184fca30f9250e1f6b6331535c1a456cf854a9a3
Tenzin Kunchok <145247192+Tenzijn@users.noreply.github.com>
2024-02-05 15:11:38 +0100

    comments removed

--- hackyourtemperature/server.js
+++ hackyourtemperature/server.js
@@ -2,16 +2,16 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
-app.use(express.json()); // Add this line to parse JSON data
+app.use(express.json()); 
 
 app.get('/', (req, res) => {
   res.send('Hello from backend to frontend :)');
 });
 
 app.post('/weather', (req, res) => {
-  // Change the route to POST and set the endpoint to /weather
-  const { cityName } = req.body; // Extract the cityName from the request body
-  res.send(cityName); // Send the cityName back as a response
+ 
+  const { cityName } = req.body; 
+  res.send(cityName); 
 });
 
 app.listen(PORT, () => {
